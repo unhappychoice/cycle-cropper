6527e10e8ad21c84793b28d675e644f9a30e7066
Yuji Ueki <unhappychoice@gmail.com>
2017-11-28 00:09:03 +0900

    Fix event names

--- src/model.ts
+++ src/model.ts
@@ -1,4 +1,5 @@
 import Cropper = require("cropperjs");
+import {CropperCropEvent, CropperCropStepEvent, CropperZoomEvent} from "cropperjs";
 import Stream from "xstream";
 import sampleCombine from "xstream/extra/sampleCombine";
 import {Actions} from "./intent";
@@ -22,18 +23,18 @@ export const model = (actions: Actions): Stream<State> => {
             const cropStart$ = Stream.never();
             const zoom$ = Stream.never();
 
-            const crop = (e) => crop$.shamefullySendNext(e);
-            const cropEnd = (e) => cropEnd$.shamefullySendNext(e);
-            const cropMove = (e) => cropMove$.shamefullySendNext(e);
-            const cropStart = (e) => cropStart$.shamefullySendNext(e);
-            const zoom = (e) => zoom$.shamefullySendNext(e);
+            const crop = (e: CropperCropEvent) => crop$.shamefullySendNext(e);
+            const cropend = (e: CropperCropStepEvent) => cropEnd$.shamefullySendNext(e);
+            const cropmove = (e: CropperCropStepEvent) => cropMove$.shamefullySendNext(e);
+            const cropstart = (e: CropperCropStepEvent) => cropStart$.shamefullySendNext(e);
+            const zoom = (e: CropperZoomEvent) => zoom$.shamefullySendNext(e);
 
             const cropper = new Cropper(element, {
                 ...props,
                 crop,
-                cropEnd,
-                cropMove,
-                cropStart,
+                cropend,
+                cropmove,
+                cropstart,
                 zoom
             });
 
